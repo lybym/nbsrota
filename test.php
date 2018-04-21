a32b257e7f066bba9608cd0ccae49b15aaf1ff32
lybym <lybym@outlook.com>
2018-04-22 01:01:08 +0800

    写了datepicker的识别

--- test.php
+++ test.php
@@ -2,21 +2,10 @@
 require('functionlib.php');
 $current_file = basename(__FILE__);
 
-$get_year=2018;
-$get_month=3;
-
-$d1=mktime(0, 0, 0, $get_month, 1, $get_year);//月初第一天
-$SearchStartDay=date("Y-m-d", $d1);
-$d2=mktime(0, 0, 0, $get_month+1, 1, $get_year)-1;//月+1第一天减一秒
-$SearchFinishDay=date("Y-m-d", $d2);
-//echo $SearchStartDay.'->'.$SearchFinishDay."<br>";
-$sdateid=dateidcreate($SearchStartDay);
-$edateid=dateidcreate($SearchFinishDay);
-
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
         <meta charset="utf-8">
         <title><?php echo $current_file;?></title>
         <link rel="stylesheet" href="css/jquery-ui.min.css">
@@ -27,33 +16,72 @@ $edateid=dateidcreate($SearchFinishDay);
             
         </style>
 
-        <script>
-        
-        </script>
-    </head>
+<script>
+function echo(display){
+    var x = document.getElementById("echo");
+	x.innerHTML=display;
+}
 
-    <body>
-        <p>日期：<input type="text" id="datepicker"></p>
+function dateidCreate(dateinfo){//将XXXX-XX-XX格式日期转化为自2014/12/29以来的天数dateid
+    // Set the unit values in milliseconds.
+    var msecPerMinute = 1000 * 60;
+    var msecPerHour = msecPerMinute * 60;
+    var msecPerDay = msecPerHour * 24;
+    var dateidZero = new Date('2014-12-29');
+    var dateidZeroMsec = dateidZero.getTime();
+    var dateidNow=new Date(dateinfo);
+    var dateidMsec=dateidNow.getTime();
+    var dateid=(dateidMsec-dateidZeroMsec)/msecPerDay+1;//因为2014/12/29的dateid就是1
+    return dateid;
+}  
 
-        <script>
-        $(function() {
-            $( "#datepicker" ).datepicker({
-                changeMonth: true,
-                changeYear: true,
-                minDate: new Date(2015, 1 - 1, 1), 
-                maxDate: "+2Y"
-            });
-        });
+function mStartDate(dateinfo){//根据月份信息获取月初日期
+    var str=dateinfo.substr(0,7);
+    var mstart=str+'-01';
+    return mstart;
+}
 
-        var sdateid='<?php echo $sdateid?>';
-        var edateid='<?php echo $edateid?>';
-        var dutynum='24';
+function mEndDate(dateinfo){//根据月份信息获取月末的日期
+    var str=mStartDate(dateinfo);
+    var mEnd = new Date(str);
+    mEnd.setMonth(mEnd.getMonth() + 1);
+    mEnd.setDate(mEnd.getDate() - 1);
+    return mEnd;
+}
 
-        var allrotainfo
+</script>
+</head>
+
+<body>
+<div id=echo></div>
+<p>请选择需要查询的日期：<input type="text" id="datepicker"></p>
+<button id="btn1">提交</button>
+
+
+
+<script>
+$(document).ready(function(){
+    $("#datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        minDate: new Date(2014,12-1,29), //起始从2015年1月1日开始
+        maxDate: "+2Y" //多显示之后2年
+    });
+
+    $("#btn1").click(function(){
+        var dpget = $("#datepicker").val();
+        sdateid=dateidCreate(mStartDate(dpget));
+        edateid=dateidCreate(mEndDate(dpget));
+        console.log(sdateid);
+        console.log(edateid);
         $.getJSON('getrotainfo.php', {'sdateid': sdateid, 'edateid': edateid}, function(data) {
-            allrotainfo=data;
+            var allrotainfo=data;
             console.log(allrotainfo);
         });
-        </script>
-    </body>
+    });
+ 
+
+});
+</script>
+</body>
 </html>
